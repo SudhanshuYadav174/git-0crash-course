cc59c8cd4aa17d48ffb977586bbb5a08f6d87345
Sudhanshu yadav <sudhanshuyadav174@gmail.com>
2025-01-25 15:04:26 +0530

    Code Changed

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const age = 24;
-const name = 'Sudhanshu Yadav';
+// const name = 'Sudhanshu Yadav';
+const name1 = 'Sudhanshu Yadav';
+
 const dob = '13/02/2004';
 
 function sayname(){
     console.log('My name id ' + name);
+}
+
+function isAbove18(){
+    return age >= 18;
 }
