db518a56dd3b0aeec917f22925e69206bd420a5e
Sudhanshu yadav <sudhanshuyadav174@gmail.com>
2025-01-25 17:17:45 +0530

    new feature

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ function sayname(){
     console.log('My name id ' + name);
 }
 
-<<<<<<< HEAD
 function newFn(){
     console.log("hey I am here");
-=======
-function isAbove18(){
-    return age >= 18;
->>>>>>> parent of 1fe95d2 (Removed 18 fn)
+}
+function newFeaturen(){
+    console.log("hey  iiiiiiI am here");
 }
