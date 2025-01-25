df7cae3dd42850908ac9f9cc3d8ed47c4d265f2c
Sudhanshu yadav <sudhanshuyadav174@gmail.com>
2025-01-25 15:34:52 +0530

    Rvert something

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const age = 24;
+// const name = 'Sudhanshu Yadav';
 const name1 = 'Sudhanshu Yadav';
 
 const dob = '13/02/2004';
@@ -7,6 +8,11 @@ function sayname(){
     console.log('My name id ' + name);
 }
 
+<<<<<<< HEAD
 function newFn(){
     console.log("hey I am here");
+=======
+function isAbove18(){
+    return age >= 18;
+>>>>>>> parent of 1fe95d2 (Removed 18 fn)
 }
